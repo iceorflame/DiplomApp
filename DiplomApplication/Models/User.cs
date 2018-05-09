90eaf668d15d88bc84db5e15565e8e04cee697ae
iceorflame <iceorflame@gmail.com>
2018-05-10 00:32:05 +0300

    Add Page
    Add Page

--- DiplomApplication/Models/User.cs
+++ DiplomApplication/Models/User.cs
@@ -10,5 +10,40 @@ namespace DiplomApplication.Models
         public int UserId { get; set; }
         public string UserLogin { get; set; }
         public string UserPassword { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderName { get; set; }
+        public DateTime OrderOut { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int PriorityId { get; set; }
+        public Priority Priority { get; set; }
+    }
+
+    public class File
+    {
+
+    }
+
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+
+    public class Priority
+    {
+        public int PriorityId { get; set; }
+        public string PriorityName { get; set; }
     }
 }
